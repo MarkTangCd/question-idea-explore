d8c156f02a6ba2539e0f838c4cfec21ac8ab9fb7
JayZhouCom <113875840+JayZhouCom@users.noreply.github.com>
2022-10-14 21:50:45 +0800

    done

--- nx-project/org/apps/todos/src/app/app.tsx
+++ nx-project/org/apps/todos/src/app/app.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Button, Input, Text } from '@chakra-ui/react';
+import { Button, Input, Text, useToast } from '@chakra-ui/react';
 import { Todo } from '@org/data';
 import { Todos } from '@org/ui';
 
@@ -7,7 +7,10 @@ import styled from './app.module.css';
 
 export const App = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [finished, setFinished] = useState<string[]>([]);
+  const [deleted, setDeleted] = useState<string[]>([]);
   const [text, setText] = useState('');
+  const toast = useToast();
 
   useEffect(() => {
     fetch('/api/todos')
@@ -15,7 +18,27 @@ export const App = () => {
       .then(setTodos);
   }, []);
 
-  function addTodo() {
+  const onDelete = (title: string) => {
+    const newArr = deleted.concat([title]);
+    setDeleted(newArr);
+    toast({
+      title: 'Note:',
+      description: 'This todo deleted',
+      status: 'success'
+    });
+  }
+
+  const onFinished = (title: string) => {
+    const newArr = finished.concat([title]);
+    setFinished(newArr);
+    toast({
+      title: 'Note:',
+      description: 'This todo is done',
+      status: 'success'
+    });
+  }
+
+  const addTodo = () => {
     fetch('/api/addTodo', {
       method: 'POST',
       headers: {
@@ -28,15 +51,20 @@ export const App = () => {
       .then((_) => _.json())
       .then((newTodo) => {
         setTodos([...todos, newTodo]);
+        setText('');
       });
+  }
+
+  const onTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // setText(e.target.value);
   }
 
   return (
     <div className={styled['wrapper']}>
       <Text fontSize='6xl'>Todos</Text>
-      <Todos todos={todos} />
+      <Todos todos={todos} finished={finished} deleted={deleted} onDelete={onDelete} onFinish={onFinished} />
       <div className={styled['todo-text']}>
-        <Input value={text} onChange={(e) => setText(e.target.value)} placeholder='Please input the text of todo' />
+        <Input value={text} onChange={onTextChange} placeholder='Please input the text of todo' />
       </div>
       <Button id={'add-todo'} onClick={addTodo}>
         Add Todo
--- nx-project/org/libs/ui/src/lib/todos/todos.tsx
+++ nx-project/org/libs/ui/src/lib/todos/todos.tsx
@@ -1,19 +1,38 @@
+import React from 'react';
 import { OrderedList, ListItem } from '@chakra-ui/react';
+import classNames from 'classnames';
 import { Todo } from '@org/data';
-import './todos.module.css';
+import styled from './todos.module.css';
 
 export interface TodosProps {
   todos: Todo[];
+  finished: string[];
+  deleted: string[];
+  onFinish: (title: string) => void;
+  onDelete: (title: string) => void;
 }
 
 export function Todos(props: TodosProps) {
+  console.log('render Todos component');
   return (
     <OrderedList>
-      {props.todos.map((t, index) => (
-        <ListItem key={index} className={'todo'}>{t.title}</ListItem>
-      ))}
+      {props.todos.map((t) => {
+        const styles = ['todo'];
+        if (props.deleted.includes(t.title)) {
+          styles.push(styled['deleted']);
+        } else if (props.finished.includes(t.title)) {
+          styles.push(styled['finished']);
+        }
+
+        return (
+          <ListItem key={t.title} className={classNames(styles)} onClick={() => props.onFinish(t.title)}>{t.title}<span onClick={(e) => {
+            e.stopPropagation();
+            props.onDelete(t.title);
+          }}>X</span></ListItem>
+        )
+      })}
     </OrderedList>
   );
 }
 
-export default Todos;
+export default React.memo(Todos);
